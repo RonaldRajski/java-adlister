d1fd83c7bf5b9bfdc9bfe6cc832c2179fef1d0a5
Ronald Rajski <ronaldrajski@gmail.com>
2021-03-07 00:38:02 -0600

    updated hello world servlet received exped error - updated navbar

--- src/main/java/expressionServlet.java
+++ src/main/java/expressionServlet.java
@@ -1,60 +1,62 @@
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-public class expressionServlet {
-}
-
-
-import java.io.*;
-        import javax.servlet.*;
-        import javax.servlet.annotation.WebServlet;
-        import javax.servlet.http.*;
-
-
-
-
-
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
-
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.io.*;
+//import javax.servlet.*;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.*;
+//
+//
+//
+//public class expressionServlet {
+//}
+//
+//
+//
+//
+//
+//
+//
+//@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+//public class HelloWorldServlet1 extends HttpServlet {
+//
+////    @Override
+////    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+////        response.setContentType("text/html");
+////        PrintWriter out = response.getWriter();
+////        out.println("<h1>Hello, World!</h1>");
+////    }
+//
+//    int pageViewCount;
+//
+//    public void init(){
+//        pageViewCount  = 0;
+//    }
+//
+//
+//
 //    @Override
 //    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String name = request.getParameter("name");
 //        response.setContentType("text/html");
+//        pageViewCount++;
 //        PrintWriter out = response.getWriter();
-//        out.println("<h1>Hello, World!</h1>");
+//        if(name !=null){
+//            pageViewCount = 0;
+//            out.printf("<h1>Hello %s</h1>", name);
+//        }
+//        else{
+//            out.println("<h1>Hello, World</h1>");
+//        }
+//        String youVisited = "You visited my page:";
+//        out.print("<h2>" + youVisited + "<h2>" + pageViewCount +"<h2/>"+ "<h2>times!</h2>");
+//
+//
+//
 //    }
-
-    int pageViewCount;
-
-    public void init(){
-        pageViewCount  = 0;
-    }
-
-
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("name");
-        response.setContentType("text/html");
-        pageViewCount++;
-        PrintWriter out = response.getWriter();
-        if(name !=null){
-            pageViewCount = 0;
-            out.printf("<h1>Hello %s</h1>", name);
-        }
-        else{
-            out.println("<h1>Hello, World</h1>");
-        }
-        String youVisited = "You visited my page:";
-        out.print("<h2>" + youVisited + "<h2>" + pageViewCount +"<h2/>"+ "<h2>times!</h2>");
-
-
-
-    }
-
-}
+//
+//}
--- src/webapp/partials/navbar.jsp
+++ src/webapp/partials/navbar.jsp
@@ -1,14 +1,10 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <nav>
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Ads</a></li>
-        <li><a href="#">Careers</a></li>
-    </ul>
-    <form action="" method="get">
-        <label for="term">Term:</label>
-        <input id="term" type="text" name="term">
-        <button>Search</button>
-    </form>
-
+    <div class="nav-wrapper">
+        <a href="#" class="brand-logo">Kalypso-Adlister</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="/profile.jsp">Profile</a></li>
+            <li><a href="/login.jsp">Login</a></li>
+        </ul>
+    </div>
 </nav>
