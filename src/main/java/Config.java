a0eb0f22d8ec1858f98e3b74bcb1c98115f5faf0
Ronald Rajski <ronaldrajski@gmail.com>
2021-03-12 16:00:56 -0600

    created database

---
+++ src/main/java/Config.java
@@ -0,0 +1,13 @@
+public class Config {
+    public String getUrl() {
+        return "jdbc:mysql://locathost/codeup_test_db?serverTimezone=UTC&useSSL=false";
+    }
+        public String getUser () {
+            return "chief";
+        }
+        public String getPassword () {
+            return "chief";
+
+        }
+    }
+
